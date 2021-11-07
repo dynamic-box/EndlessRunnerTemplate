7aa328038b149315d93a58a839142e35b2502bca
VaqifQuluzadaUnity <60698872+VaqifQuluzadaUnity@users.noreply.github.com>
2021-11-08 03:22:36 +0400

    Shop System Complete Commented

--- AnimationTemplate/Assets/ItemController.cs
+++ AnimationTemplate/Assets/ItemController.cs
@@ -5,23 +5,42 @@ using UnityEngine.UI;
 
 public class ItemController : MonoBehaviour
 {
+    //Item haqqinda melumati dasiyan scriptable object instance
     [SerializeField] private ItemDataSO itemData;
 
+    //Itemi almaq ve secmek ucun istifade etdiyimiz button
     [SerializeField] private Button buyOrSelectButton;
 
+    //button uzerindeki(Buy Selected Select) yazdiracagimiz text
     [SerializeField] private Text buyOrSelectText;
 
+    //Itemin qiymeti yazilacaq text
+    [SerializeField] private Text itemPriceText;
+
+    //Itemin seklini yerlestimek ucun Image
+    [SerializeField] private Image itemIcon;
+
+    //Ve item-in indeksi
     public int itemIndex;
 
 
     private void Start()
     {
+        //Itemin sekil ve price qiymetini teyin edirik.
+        itemIcon.sprite = itemData.itemImage;
+
+        itemPriceText.text = itemData.itemPrice.ToString();
+
+        //Eger item alinibsa
         if (itemData.isBought)
         {
+            //Buttona sadece Select funksionalligi elave edirik.
             buyOrSelectButton.onClick.AddListener(onSelectButtonPressed);
 
+            //Eger bizim item evvelceden secilibse
             if (itemData.isSelected)
             {
+                //Texti deyisirik.
                 buyOrSelectText.text = "Selected";
             }
             else
@@ -31,6 +50,7 @@ public class ItemController : MonoBehaviour
         }
         else
         {
+            //Buttona sadece buy funksionalligini elave etdik.
             buyOrSelectButton.onClick.AddListener(onBuyButtonPressed);
 
             buyOrSelectText.text = "Buy";
@@ -40,29 +60,46 @@ public class ItemController : MonoBehaviour
 
     private void onBuyButtonPressed()
     {
+        //Eger pulumuz catarsa bu zaman true qaytarir
         if (PlayerManager.instance.onBuyButtonPressed(itemData.itemPrice))
         {
+            //Itemi alir ve secir.
             itemData.isBought = true;
 
             itemData.isSelected = true;
 
             buyOrSelectText.text = "Selected";
+
+            //Artiq buy event-na ehtiyacimiz yoxdu deye kenarlasdirdiq.
+            buyOrSelectButton.onClick.RemoveAllListeners();
+
+            //Hemin buttonu metodla secirik.
+            onSelectButtonPressed();
+
+            //Ve secim etme funksionalligini elave edirik.
+            buyOrSelectButton.onClick.AddListener(onSelectButtonPressed);
         }
 
-        buyOrSelectButton.onClick.RemoveListener(onBuyButtonPressed);
 ;    }
 
     private void onSelectButtonPressed()
     {
+        //Biz yeni item secende evvelki item-i resetleyirik.
         PlayerManager.instance.onSelectButtonPressed(itemIndex);
 
+        //Indiki item-i secirik.
         itemData.isSelected = true;
+
+        //Ve buttonun text-ne selected yazisini elave edirik.
+        buyOrSelectText.text = "Selected";
     }
 
     public void unSelect()
     {
+        //Bu metodu PlayerManagerin singletonundaki onSelectButtonPressed EVVELKI ITEM ucun cagirir.
         itemData.isSelected = false;
 
+        //Ve EVVELKI ITEM-in text-ni deyisirik.
         buyOrSelectText.text = "Select";
     }
 }
--- AnimationTemplate/Assets/PlayerManager.cs
+++ AnimationTemplate/Assets/PlayerManager.cs
@@ -1,12 +1,16 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    //Singleton ucun reference
     public static PlayerManager instance;
+
+    //playerin datalarinin(coin ve selected item indexi) idare etmek ucun reference
     [SerializeField] private PlayerDataSO playerData;
 
+    //Butun itemController scriptlərin SIRA ilə düzülmüş arrayı
     [SerializeField] private ItemController[] itemsList;
 
     
@@ -18,14 +22,13 @@ public class PlayerManager : MonoBehaviour
         instance = this;
     }
 
-    private void Start()
-    {
-        
-    }
+    //Bu metodla biz playerin item-a pulunun catib catmadigini yoxlayiriq.
     public bool onBuyButtonPressed(int itemPrice)
     {
+        //Itemin qiymeti ile playerin coin sayini muqayise edirik.
         if (playerData.GetCoin() >= itemPrice)
         {
+            //Eger playerin coin sayi uygundursa itemin qiymetini playerin coin sayindan cixiriq.
             playerData.SetCoin(playerData.GetCoin() - itemPrice);
             return true;
         }
@@ -34,8 +37,10 @@ public class PlayerManager : MonoBehaviour
 
     public void onSelectButtonPressed(int itemIndex)
     {
+        //Bu indeks vasitesile yuxaridaki itemListin indekslerine gore evvelki secdiyimiz elementi deselect edirik.
         itemsList[playerData.currentSelectedItemIndex].unSelect();
 
+        //ve hazirki secidiyimiz elementin indexini teyin edirik.
         playerData.currentSelectedItemIndex = itemIndex;
 
 
